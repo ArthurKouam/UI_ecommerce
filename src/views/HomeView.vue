1864632be123d42cfbf6d5a4ac056402eb558dab
Arthur Kouam <164850622+ArthurKouam@users.noreply.github.com>
2024-09-20 18:05:50 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
 <template>
     <section class=" w-full fixed inset-0 overflow-hidden">
         <img src="@/assets/images/image4.png" class="w-full h-full overflow-hidden" alt="">
-        <h1 class=" absolute top-48 left-1/2 -translate-x-1/2 font-black text-center w-64 text-4xl text-white">Rebuild your interior</h1>
+        <h1 class=" absolute top-44 left-1/2 -translate-x-1/2 font-black text-center w-64 text-4xl text-white">Rebuild your interior</h1>
         <router-link to="/catalog">
             <button class=" absolute but left-1/2 -translate-x-1/2 bg-white rounded-xl font-bold">Go to Catalog</button>
         </router-link>
@@ -15,7 +15,7 @@
 
 <style scoped>
     .but{
-        top: 513px;
+        top: 413px;
         width: 165px;
         height: 51px;
     }
