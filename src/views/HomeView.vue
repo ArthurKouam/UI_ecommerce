e206a2e156f81b563cfb2dfbdc05710aa4b7ca8e
Arthur Kouam <164850622+ArthurKouam@users.noreply.github.com>
2024-09-20 17:48:51 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <section class=" w-full">
-        <img src="@/assets/images/image4.png" class="w-full h-screen" alt="">
+    <section class=" w-full fixed inset-0">
+        <img src="@/assets/images/image4.png" class="w-full h-screen overflow-hidden" alt="">
         <h1 class=" absolute top-48 left-1/2 -translate-x-1/2 font-black text-center w-64 text-4xl text-white">Rebuild your interior</h1>
         <router-link to="/catalog">
             <button class=" absolute but left-1/2 -translate-x-1/2 bg-white rounded-xl font-bold">Go to Catalog</button>
